d847f34aa740181bfef6da20c93e37e20443a4b0
Michael E. Cotterell <mepcotterell@gmail.com>
2019-01-12 12:37:16 -0500

    updated wording

--- src/cs1302/exceptions/Printer.java
+++ src/cs1302/exceptions/Printer.java
@@ -1,10 +1,19 @@
 package cs1302.exceptions;
 
 /**
- *
+ * A utility class for printing lines from files using <code>Scanner</code> objects.
  */
 public class Printer {
 
+    /**
+     * Prints lines using the given <code>Scanner</code> object.
+     */
+    public static void printLines(Scanner s) {
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            System.out.println(line);
+        } // while
+    } // print
 
 } // Printer
 

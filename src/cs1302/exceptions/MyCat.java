8f11911e6e69379385b363493028d160d6e3dc57
Michael E. Cotterell <mepcotterell@gmail.com>
2019-01-12 12:30:46 -0500

    updated wording

--- src/cs1302/exceptions/MyCat.java
+++ src/cs1302/exceptions/MyCat.java
@@ -1,12 +1,18 @@
 package cs1302.exceptions;
 
 /** 
- *
+ * A simpler version of the Unix <code>cat</code> command.
  */
 public class MyCat {
 
     /**
+     * Entry point for the application. Exactly zero or one command-line arguments are expected.
+     * If a filename is given as an argument, then the program should print the contents of that
+     * program to standard output. If a single dash (i.e., "-") is given as an argument, then
+     * the program should print the contents of standard input. If no arguments are given, then
+     * the program should work the same as if "-" given.
      *
+     * @param args  the command-line arguments
      */
     public static void main(String args) {
 
